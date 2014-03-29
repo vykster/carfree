dc6667bf0ade9584ff5883f18e9c26cf723d47cf
Vyki <vyki@nationbuilder.com>
2014-03-29 13:22:40 -0700

    add header

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Carfree::Application.routes.draw do
+  get "pages/info"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'pages#info'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
