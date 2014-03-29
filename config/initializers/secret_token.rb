ac6b2a403fc387d5a7ef31e0533d053df27269e8
Vyki <vyki@nationbuilder.com>
2014-03-29 12:30:25 -0700

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Carfree::Application.config.secret_key_base = '7273f145cd67cd093ea0354d6def78159bee06a885ff45295d7e4aac25a648feee9cf52ca09eb04cc126a04ac6a236d544a1b5317c304f0d94556bfbef6d6bd8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Carfree::Application.config.session_store :cookie_store, key: '_carfree_session'
